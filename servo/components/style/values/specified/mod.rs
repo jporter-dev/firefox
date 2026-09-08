b4f027ffe395c129063106a76fe845d1555838d2
kevindoughty <kvndy@protonmail.com>
2026-09-08 20:01:46 +0000

    Bug 2066417 - [animation] Opacity clamping rollback r=firefox-style-system-reviewers,dshin
    Differential Revision: https://phabricator.services.mozilla.com/D323334

--- servo/components/style/values/specified/mod.rs
+++ servo/components/style/values/specified/mod.rs
@@ -344,9 +344,9 @@ impl ToComputedValue for Opacity {
     #[inline]
     fn to_computed_value(&self, context: &Context) -> CSSFloat {
         let value = self.0.to_computed_value(context).value();
-        if context.for_animation {
-            // Type <number> and <percentage> should be able to interpolate
-            // out-of-range opacity values which benefits additive animation
+        if context.for_smil_animation {
+            // SMIL expects to be able to interpolate between out-of-range
+            // opacity values.
             value
         } else {
             value.min(1.0).max(0.0)
